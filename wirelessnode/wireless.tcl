0b79fa30cdb57e51584e61a82df57c4f7c1bfad5
johanericka <johanericka@gmail.com>
2014-12-24 23:40:56 +0700

    10wnodes broadcast_ok

--- wirelessnode/wireless.tcl
+++ wirelessnode/wireless.tcl
@@ -108,6 +108,12 @@ set n(1) [$ns node]
 set n(2) [$ns node]
 set n(3) [$ns node]
 set n(4) [$ns node]
+set n(5) [$ns node]
+set n(6) [$ns node]
+set n(7) [$ns node]
+set n(8) [$ns node]
+set n(9) [$ns node]
+
 $n(0) set X_ 1000
 $n(0) set Y_ 100
 $n(0) set Z_ 0
@@ -123,33 +129,65 @@ $n(3) set Z_ 0
 $n(4) set X_ 200
 $n(4) set Y_ 100
 $n(4) set Z_ 0
+$n(5) set X_ 900
+$n(5) set Y_ 300
+$n(5) set Z_ 0
+$n(6) set X_ 700
+$n(6) set Y_ 300
+$n(6) set Z_ 0
+$n(7) set X_ 500
+$n(7) set Y_ 300
+$n(7) set Z_ 0
+$n(8) set X_ 300
+$n(8) set Y_ 300
+$n(8) set Z_ 0
+$n(9) set X_ 100
+$n(9) set Y_ 300
+$n(9) set Z_ 0
+
 
 $ns initial_node_pos $n(0) 100
 $ns initial_node_pos $n(1) 100
 $ns initial_node_pos $n(2) 100
 $ns initial_node_pos $n(3) 100
 $ns initial_node_pos $n(4) 100
+$ns initial_node_pos $n(5) 100
+$ns initial_node_pos $n(6) 100
+$ns initial_node_pos $n(7) 100
+$ns initial_node_pos $n(8) 100
+$ns initial_node_pos $n(9) 100
 
 # attach a new Agent/MessagePassing/Flooding to each node on port $MESSAGE_PORT
 set a(0) [new Agent/MessagePassing/Flooding]
 $n(0) attach $a(0) $MESSAGE_PORT
 $a(0) set messages_seen {}
-
 set a(1) [new Agent/MessagePassing/Flooding]
 $n(1) attach $a(1) $MESSAGE_PORT
 $a(1) set messages_seen {}
-
 set a(2) [new Agent/MessagePassing/Flooding]
 $n(2) attach $a(2) $MESSAGE_PORT
 $a(2) set messages_seen {}
-
 set a(3) [new Agent/MessagePassing/Flooding]
 $n(3) attach $a(3) $MESSAGE_PORT
 $a(3) set messages_seen {}
-
 set a(4) [new Agent/MessagePassing/Flooding]
 $n(4) attach $a(4) $MESSAGE_PORT
 $a(4) set messages_seen {}
+set a(5) [new Agent/MessagePassing/Flooding]
+$n(5) attach $a(5) $MESSAGE_PORT
+$a(5) set messages_seen {}
+set a(6) [new Agent/MessagePassing/Flooding]
+$n(6) attach $a(6) $MESSAGE_PORT
+$a(6) set messages_seen {}
+set a(7) [new Agent/MessagePassing/Flooding]
+$n(7) attach $a(7) $MESSAGE_PORT
+$a(7) set messages_seen {}
+set a(8) [new Agent/MessagePassing/Flooding]
+$n(8) attach $a(8) $MESSAGE_PORT
+$a(8) set messages_seen {}
+set a(9) [new Agent/MessagePassing/Flooding]
+$n(9) attach $a(9) $MESSAGE_PORT
+$a(9) set messages_seen {}
 
 
 #broadcast
